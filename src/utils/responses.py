91cbd1a42d2f6ed8b30b55c9a7560be8d7fa7ffe
dandroid <mail@dandroid.app>
2024-02-19 17:50:20 -0600

    refactored responses code to be more readable

--- src/utils/responses.py
+++ src/utils/responses.py
@@ -21,8 +21,8 @@ _last_known_good_file = None
 def load_responses_file():
     global _keyword_lists, _response_lists, _last_known_good_file
 
-    keyword_lists_old = _keyword_lists
-    response_lists_old = _response_lists
+    keyword_lists_old = _keyword_lists.copy()
+    response_lists_old = _response_lists.copy()
 
     _keyword_lists = []
     _response_lists = []
@@ -31,41 +31,40 @@ def load_responses_file():
         with open(RESPONSES_FILE_PATH, "r") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
-                keyword_list = []
-                for keyword in row[0].split(";;"):
-                    keyword = keyword.strip().lower()
-                    if keyword == "":
-                        raise ValueError("Keyword cannot be empty.")
-                    if keyword.startswith('r"'):
-                        if keyword.endswith('/i"'):
-                            keyword = re.compile(fr"{keyword[2:-3]}", re.IGNORECASE)
-                        else:
-                            keyword = re.compile(fr"{keyword[2:-1]}")
-                    keyword_list.append(keyword)
-                _keyword_lists.append(keyword_list)
-                response_list = []
-                for response in row[1].split(";;"):
-                    response = response.strip()
-                    if response == "":
-                        raise ValueError("Response cannot be empty.")
-                    if response.startswith('\\'):
-                        response = response[1:]
-                    response_list.append(response)
-                _response_lists.append(response_list)
+                _keyword_lists.append([parse_keyword(keyword) for keyword in row[0].split(";;")])
+                _response_lists.append([parse_response(response) for response in row[1].split(";;")])
             csv_file.seek(0)
             _last_known_good_file = csv_file.read().replace("\\", "\\\\\\")
-
-        # with open(RESPONSES_FILE_PATH) as csv_file:
-        #     _last_known_good_file = csv_file.read().replace("\\", "\\\\\\")
         logger.i(TAG, "Successfully loaded responses file.")
-        logger.d(TAG, _keyword_lists)
-        logger.d(TAG, _response_lists)
+        logger.d(TAG, str(_keyword_lists))
+        logger.d(TAG, str(_response_lists))
         return True
     except Exception as e:
         logger.e(TAG, f"Error loading responses file: {e}")
         _keyword_lists = keyword_lists_old
         _response_lists = response_lists_old
         return False
+
+
+def parse_keyword(keyword: str) -> str | re.Pattern:
+    keyword = keyword.strip().lower()
+    if keyword == "":
+        raise ValueError("Keyword cannot be empty.")
+    if keyword.startswith('r"'):
+        if keyword.endswith('/i"'):
+            keyword = re.compile(fr"{keyword[2:-3]}", re.IGNORECASE)
+        else:
+            keyword = re.compile(fr"{keyword[2:-1]}")
+    return keyword
+
+
+def parse_response(response: str) -> str:
+    response = response.strip()
+    if response == "":
+        raise ValueError("Response cannot be empty.")
+    if response.startswith('\\'):
+        response = response[1:]
+    return response
 
 
 def get_all_responses():
@@ -88,21 +87,14 @@ def get_response(message: str) -> Optional[str]:
     """
     if message is None:
         return None
-    global _keyword_lists, _response_lists 
+
+    global _keyword_lists, _response_lists
     for index, keyword_list in enumerate(_keyword_lists):
         for keyword in keyword_list:
-            if isinstance(keyword, str):
-                if keyword in message.lower():
-                    return random.choice(_response_lists[index])
-            elif type(keyword) is re.Pattern:
-                match = keyword.search(message.lower())
-                if not match:
-                    continue
-                # TODO: this is kind of lazy. Figure out a better way to do this, because if more than one keyword uses groups, it will not work
-                if hasattr(match, "groups") and len(match.groups()) > 0:
-                    return f"{match.group(1).title()} {random.choice(_response_lists[index])}"
-                else:
-                    return random.choice(_response_lists[index])
+            if isinstance(keyword, str) and keyword in message.lower():
+                return random.choice(_response_lists[index])
+            elif isinstance(keyword, re.Pattern) and keyword.search(message.lower()):
+                return random.choice(_response_lists[index])
     return None
 
 
